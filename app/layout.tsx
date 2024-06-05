fb7baa4be83b7c0f93acb083cb106230a3f8fe35
DvaDvaD <davidlou0810@gmail.com>
2024-06-05 17:00:29 +0700

    :sparkles: Add progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,36 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
-import { ModalProvider } from '@/providers/modal-provider'
-import { ToastProvider } from '@/providers/toast-provider'
-import { ThemeProvider } from '@/providers/theme-provider'
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToastProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 
-import './globals.css'
+import "./globals.css";
+import ProgressBarProvider from "@/components/progress-bar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Dashboard',
-  description: 'E-Commerce Dashboard',
-}
+  title: "Dashboard",
+  description: "E-Commerce Dashboard",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider 
-            attribute="class" 
-            defaultTheme="system" 
-            enableSystem
-          >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <ToastProvider />
             <ModalProvider />
-            {children}
+            <ProgressBarProvider>{children}</ProgressBarProvider>
           </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
